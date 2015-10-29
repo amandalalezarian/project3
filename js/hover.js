38bb5e06724933c37927c1013eda7d1493da8a64
amandalalezarian <amandalalezarian@gmail.com>
2015-10-28 23:17:16 -0400

    final

--- js/hover.js
+++ js/hover.js
@@ -6,35 +6,74 @@
          $(this).qtip({
              content: {
                  text: $(this).next('.tooltiptext')
-             }
+             },
+             position: {
+                 my: 'top right',  // Position my top left...
+             },
+             style: {
+                classes: 'qtip-blue qtip-rounded'
+              }
          });
+
      });
+
      $('#neoplasms').each(function() {
          $(this).qtip({
              content: {
                  text: $(this).next('.tooltiptext')
-             }
+             },
+             position: {
+                 my: 'top right',  // Position my top left...
+             },
+             style: {
+                classes: 'qtip-blue qtip-rounded'
+              }
          });
+
      });
+
      $('#flu').each(function() {
          $(this).qtip({
              content: {
                  text: $(this).next('.tooltiptext')
-             }
+             },
+             position: {
+                 my: 'top right',  // Position my top left...
+             },
+             style: {
+                classes: 'qtip-blue qtip-rounded'
+              }
          });
+
      });
+
      $('#diabetes').each(function() {
          $(this).qtip({
              content: {
                  text: $(this).next('.tooltiptext')
-             }
+             },
+             position: {
+                 my: 'top right',  // Position my top left...
+             },
+             style: {
+                classes: 'qtip-blue qtip-rounded'
+              }
          });
+
      });
+
      $('#respiratory').each(function() {
          $(this).qtip({
              content: {
                  text: $(this).next('.tooltiptext')
-             }
+             },
+             position: {
+                 my: 'top right',  // Position my top left...
+             },
+             style: {
+                classes: 'qtip-blue qtip-rounded'
+              }
          });
      });
+
  });
